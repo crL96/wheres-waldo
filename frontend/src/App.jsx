25b3eaa38217682c5861cf459e785ab64c9770c9
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-05 14:05:45 +0200

    Add Where's Waldo image and function that returns normalized coordinates for clicks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-
+import waldoImg from "./assets/wheres-waldo.avif";
 import './App.css'
 
 function App() {
 
+  function getNormalizedCoordinates(e) {
+    const xAbs = e.pageX - e.target.offsetLeft;
+    const yAbs = e.pageY - e.target.offsetTop;
+
+    const xNorm = xAbs / e.target.width;
+    const yNorm = yAbs / e.target.height;
+    return { x: xNorm, y: yNorm };
+  }
+
+  function handleClick(e) {
+    const coordinates =  getNormalizedCoordinates(e);
+    console.log(coordinates);
+  }
+
   return (
     <>
       <h1>Where's Waldo?</h1>
+      <img src={waldoImg} alt="Where's Waldo" onClick={handleClick}/>
     </>
   )
 }
