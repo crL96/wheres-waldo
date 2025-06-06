285bf89c3b76b280f649388c1e5afd45838af552
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-06 12:50:13 +0200

    Add score model for leaderboard to db

---
+++ backend/prisma/migrations/20250606104941_leaderboard/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Score" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "timeToComplete" INTEGER NOT NULL,
+
+    CONSTRAINT "Score_pkey" PRIMARY KEY ("id")
+);
